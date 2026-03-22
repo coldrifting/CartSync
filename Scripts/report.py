ce312b7b6dae67474ae9f464a5f479aa7de8e8f0
Aiden Van Dyke <aidenavandyke@gmail.com>
2026-03-22 16:59:52 -0600

    Stop migration script from leaving incomplete data and fix macos browser behavior

--- Scripts/report.py
+++ Scripts/report.py
@@ -29,27 +29,29 @@ testResultsPath = testsPath / "TestResults"
 outputDir = Path(tempfile.gettempdir()).resolve() / "TestResults" / "Output"
 reportFile = outputDir / "Index.html"
 
-if outputDir.is_dir():
-    shutil.rmtree(outputDir)
-    print(f"Directory '{outputDir}' Deleted")
+try:
+    if outputDir.is_dir():
+        shutil.rmtree(outputDir)
+        print(f"Directory '{outputDir}' Deleted")
+    
+    subprocess.run(["dotnet",
+                             "test",
+                             str(rootDir),
+                             "--settings",
+                             str(runSettings)],
+                            check=True)
+    
+    coverageFile = str(get_first_subdir(testResultsPath) / "coverage.cobertura.xml")
+    
+    subprocess.run(["reportgenerator",
+                    f"-reports:\"{coverageFile}\"",
+                    f"-targetdir:{outputDir}",
+                    "-reporttypes:Html"],
+                   check=True)
+    
+    webbrowser.open("file://" + str(reportFile), new=0, autoraise=True)
 
-subprocess.run(["dotnet",
-                         "test",
-                         str(rootDir),
-                         "--settings",
-                         str(runSettings)],
-                        check=True)
-
-coverageFile = str(get_first_subdir(testResultsPath) / "coverage.cobertura.xml")
-
-subprocess.run(["reportgenerator",
-                f"-reports:\"{coverageFile}\"",
-                f"-targetdir:{outputDir}",
-                "-reporttypes:Html"],
-               check=True)
-
-if testResultsPath.is_dir():
-    shutil.rmtree(testResultsPath)
-    print(f"Directory '{testResultsPath}' Deleted")
-
-webbrowser.open(str(reportFile), new=0, autoraise=True)
+finally:
+    if testResultsPath.is_dir():
+        shutil.rmtree(testResultsPath)
+        print(f"Directory '{testResultsPath}' Deleted")
