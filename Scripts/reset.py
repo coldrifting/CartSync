dd00784c3d697c2f20101ac92cb489a690ee3c80
Aiden Van Dyke <aidenavandyke@gmail.com>
2026-03-25 11:33:54 -0600

    Added reset database script

---
+++ Scripts/reset.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from pathlib import Path
+from inspect import currentframe, getframeinfo
+import subprocess
+
+migrationFolder = "_Migrations"
+
+scriptFile = getframeinfo(currentframe()).filename
+rootDir = Path(scriptFile).resolve().parent.parent
+
+apiPath = rootDir / "API" / "CartSync"
+projectPath = apiPath / "CartSync.csproj"
+
+subprocess.run(["dotnet",
+                "ef",
+                "database",
+                "drop",
+                "--force",
+                "--project",
+                str(projectPath)],
+               check=True)
+
+subprocess.run(["dotnet",
+                "ef",
+                "database",
+                "update",
+                "--project",
+                str(projectPath)],
+               check=True)
