23f9888055302cb222798eae05cd13b828df7ffe
Aiden Van Dyke <aidenavandyke@gmail.com>
2026-03-25 17:02:42 -0600

    Added placeholders for cart, recipe and store routes

---
+++ Frontend/src/routes/cart/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import {enhance} from '$app/forms';
+    import type {PageProps} from './$types';
+    let {data}: PageProps = $props();
+</script>
+
+<svelte:head>
+    <title>Cart</title>
+</svelte:head>
+
+<h1>Cart</h1>
+<p>TODO</p>
+<!--TODO-->
---
+++ Frontend/src/routes/recipes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import {enhance} from '$app/forms';
+    import type {PageProps} from './$types';
+    let {data}: PageProps = $props();
+</script>
+
+<svelte:head>
+    <title>Recipes</title>
+</svelte:head>
+
+<h1>Recipes</h1>
+<p>TODO</p>
+<!--TODO-->
